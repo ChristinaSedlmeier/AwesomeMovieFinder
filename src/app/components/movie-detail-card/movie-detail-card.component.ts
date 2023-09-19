75c801f330e1c75c019a8c4deece6f301d3765dc
Christina Sedlmeier <38252323+ChristinaSedlmeier@users.noreply.github.com>
2023-09-19 08:02:14 +0200

    list-view.component, movie-card.component and search-field.component

---
+++ src/app/components/movie-detail-card/movie-detail-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MovieModel } from 'src/app/models/movieModel';
+
+@Component({
+  selector: 'movie-detail-card',
+  templateUrl: './movie-detail-card.component.html',
+  styleUrls: ['./movie-detail-card.component.scss']
+})
+export class MovieDetailCardComponent implements OnInit {
+  @Input()
+  public movie: MovieModel;
+
+  constructor() { }
+
+  public ngOnInit(): void {
+  }  
+}
